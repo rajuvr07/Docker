2bb23b964b5e498c0163c4e8a637114b4f025cb0
Pushparaj <raju.vellayil@gmail.com>
2020-05-09 15:49:55 +0530

    mm

--- MyWebApp/MyWebApp/obj/Debug/netcoreapp3.1/MyWebApp.AssemblyInfo.cs
+++ MyWebApp/MyWebApp/obj/Debug/netcoreapp3.1/MyWebApp.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("3c2cdb11-79d6-4772-abe8-29b962c400f7")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
