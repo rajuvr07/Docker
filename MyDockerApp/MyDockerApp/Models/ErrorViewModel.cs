3b50a6a988356529862ff185cfb1e750caf31a54
Pushparaj <raju.vellayil@gmail.com>
2020-05-09 13:12:38 +0530

    initail checkin

---
+++ MyDockerApp/MyDockerApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyDockerApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
